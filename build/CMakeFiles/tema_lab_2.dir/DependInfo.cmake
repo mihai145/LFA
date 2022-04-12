853b2eb75acb3da9f70e1fbbd56bb221d9f9690b
mihai145 <opreamihai01@yahoo.com>
2022-04-12 11:18:08 +0300

    change folder structure

---
+++ build/CMakeFiles/tema_lab_2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mihai145/Uni/an1-sem2/lfa/LFA/main.cpp" "/home/mihai145/Uni/an1-sem2/lfa/LFA/build/CMakeFiles/tema_lab_2.dir/main.cpp.o"
+  "/home/mihai145/Uni/an1-sem2/lfa/LFA/src/Automaton.cpp" "/home/mihai145/Uni/an1-sem2/lfa/LFA/build/CMakeFiles/tema_lab_2.dir/src/Automaton.cpp.o"
+  "/home/mihai145/Uni/an1-sem2/lfa/LFA/src/Base_Automaton.cpp" "/home/mihai145/Uni/an1-sem2/lfa/LFA/build/CMakeFiles/tema_lab_2.dir/src/Base_Automaton.cpp.o"
+  "/home/mihai145/Uni/an1-sem2/lfa/LFA/src/Partition.cpp" "/home/mihai145/Uni/an1-sem2/lfa/LFA/build/CMakeFiles/tema_lab_2.dir/src/Partition.cpp.o"
+  "/home/mihai145/Uni/an1-sem2/lfa/LFA/src/State.cpp" "/home/mihai145/Uni/an1-sem2/lfa/LFA/build/CMakeFiles/tema_lab_2.dir/src/State.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tema_lab_2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema_lab_2.dir/main.cpp.o"
+  "CMakeFiles/tema_lab_2.dir/src/Automaton.cpp.o"
+  "CMakeFiles/tema_lab_2.dir/src/Base_Automaton.cpp.o"
+  "CMakeFiles/tema_lab_2.dir/src/Partition.cpp.o"
+  "CMakeFiles/tema_lab_2.dir/src/State.cpp.o"
+  "tema_lab_2"
+  "tema_lab_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema_lab_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
